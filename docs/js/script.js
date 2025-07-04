2c5c3623b462114e89b540df0ff5cf282e91c190
chips-bean <kokannsay@gmail.com>
2025-07-04 23:05:46 +0700

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,33 +1,3 @@
-// fetch('nav_bar.html')
-//     .then(res => res.text())
-//     .then(data => {
-//         document.getElementById('nav_bar').innerHTML = data;
-//     });
-
-
-// function loadPage(page, link = null) {
-//     fetch(page)
-//         .then(result => result.text())
-//         .then(data => {
-//             document.getElementById('content').innerHTML = data;
-//         });
-
-//     if (link) {
-//         document.querySelectorAll('header nav li a').forEach(Element => {
-//             Element.classList.remove('active');
-//         });
-
-//         link.classList.add('active')
-//     }
-// }
-// window.onload = () => {
-//     const homeLink = document.querySelector('a[onclick*="home.html"]');
-//     if (homeLink) {
-//         loadPage('home.html', homeLink);
-//     }
-// };
-// Load the nav bar first
-// Load nav bar first
 fetch('nav_bar.html')
     .then(result => result.text())
     .then(data => {
