7c4ce75932f578e3a195d4ba0fb1d4908c25f442
chips-bean <kokannsay@gmail.com>
2025-07-04 21:38:28 +0700

    just test

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,28 +1,60 @@
+// fetch('nav_bar.html')
+//     .then(res => res.text())
+//     .then(data => {
+//         document.getElementById('nav_bar').innerHTML = data;
+//     });
+
+
+// function loadPage(page, link = null) {
+//     fetch(page)
+//         .then(result => result.text())
+//         .then(data => {
+//             document.getElementById('content').innerHTML = data;
+//         });
+
+//     if (link) {
+//         document.querySelectorAll('header nav li a').forEach(Element => {
+//             Element.classList.remove('active');
+//         });
+
+//         link.classList.add('active')
+//     }
+// }
+// window.onload = () => {
+//     const homeLink = document.querySelector('a[onclick*="home.html"]');
+//     if (homeLink) {
+//         loadPage('home.html', homeLink);
+//     }
+// };
 fetch('nav_bar.html')
-    .then(result => result.text())
+    .then(res => res.text())
     .then(data => {
         document.getElementById('nav_bar').innerHTML = data;
+        loadFromHash(); // load page content according to hash
     });
 
+window.onhashchange = () => {
+    loadFromHash(); 
+};
 
+function loadFromHash() {
+    let hash = window.location.hash.substr(1);
+    if (!hash) {
+        hash = 'home.html';
+        window.location.hash = '#home.html';
+    }
+    const link = document.querySelector(`a[href="#${hash}"]`);
+    loadPage(hash, link);
+}
 function loadPage(page, link = null) {
     fetch(page)
-        .then(result => result.text())
+        .then(res => res.text())
         .then(data => {
             document.getElementById('content').innerHTML = data;
         });
 
     if (link) {
-        document.querySelectorAll('header nav li a').forEach(Element => {
-            Element.classList.remove('active');
-        });
-
-        link.classList.add('active')
+        document.querySelectorAll('nav ul li a').forEach(el => el.classList.remove('active'));
+        link.classList.add('active');
     }
 }
-window.onload = () => {
-    const homeLink = document.querySelector('a[onclick*="home.html"]');
-    if (homeLink) {
-        loadPage('home.html', homeLink);
-    }
-};
